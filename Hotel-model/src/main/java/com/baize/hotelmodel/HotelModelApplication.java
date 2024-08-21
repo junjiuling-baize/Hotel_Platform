f3cb86b7f62b3488acea50b30ecc59164809f036
白泽 <1259151708@qq.com>
2024-08-21 11:25:03 +0800

    model的创建

---
+++ Hotel-model/src/main/java/com/baize/hotelmodel/HotelModelApplication.java
@@ -0,0 +1,13 @@
+package com.baize.hotelmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelModelApplication.class, args);
+    }
+
+}
---
+++ Hotel-model/src/test/java/com/baize/hotelmodel/HotelModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baize.hotelmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
